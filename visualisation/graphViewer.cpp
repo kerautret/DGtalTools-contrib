88e2a347af6b6ec49419c9313b78f6e7b24fe1de
Bertrand Kerautret <bertrand.kerautret@univ-lyon2.fr>
2023-01-31 23:59:08 +0100

    graphViewer: add an option use constant radius along a section edge

--- visualisation/graphViewer.cpp
+++ visualisation/graphViewer.cpp
@@ -94,6 +94,7 @@ int main( int argc, char** argv )
   double r {1.0};
   bool useRadiiFile {false};
   bool autoEdgeOpt {false};
+  bool cstRadSectionEdge {false};
   std::vector<unsigned int> vectColMesh;
   std::vector<unsigned int> vectColVertex;
   std::vector<unsigned int> vectColEdge;
@@ -104,6 +105,7 @@ int main( int argc, char** argv )
   app.add_option("--inputVertex,-v", nameFileVertex, "input file containing the vertex list.")->required()->check(CLI::ExistingFile);
   app.add_option("--inputEdge,-e", nameFileEdge, "input file containing the edge list.")->required()->check(CLI::ExistingFile);
   app.add_flag("--autoEdge,-a", autoEdgeOpt, "generate edge list from vertex order.");
+  app.add_flag("--cstRadSectionEdge,-s", cstRadSectionEdge,  "use constant radius along a section edge (tube representing the edges are of constant radius from the begin section until the end section of the tube.");
   auto inputRadiiOpt = app.add_option("--inputRadii,-r", nameFileRadii, "input file containing the radius for each vertex.");
   app.add_option("--ballRadius,-b", r, "radius of vertex balls.", true);
   auto addMeshOpt = app.add_option("--addMesh,-m", meshName, "add mesh in the display.");
@@ -219,7 +221,8 @@ int main( int argc, char** argv )
     {
       Mesh<Z3i::RealPoint> aMesh;
       vertex = { vectVertex[e[0]], vectVertex[e[1]] };
-      radii = { vectRadii[e[0]], vectRadii[e[1]] };
+      radii =   { cstRadSectionEdge ? std::min(vectRadii[e[0]], vectRadii[e[1]]): vectRadii[e[0]],
+                  cstRadSectionEdge ?  std::min(vectRadii[e[0]], vectRadii[e[1]]) : vectRadii[e[1]] };
       Mesh<Z3i::RealPoint>::createTubularMesh(aMesh, vertex, radii, 0.05);
       viewer << (useRadiiFile ? CustomColors3D( hueShade(radii[0]*10000), hueShade(radii[1]*10000) ) : CustomColors3D( hueShade(e[0]), hueShade(e[1]) ));
       viewer << aMesh;
@@ -231,7 +234,10 @@ int main( int argc, char** argv )
     for ( const auto& e: vectEdges )
     {
       vertex = { vectVertex[e[0]], vectVertex[e[1]] };
-      radii = { vectRadii[e[0]], vectRadii[e[1]] };
+       radii =   { cstRadSectionEdge ? std::min(vectRadii[e[0]], vectRadii[e[1]]): vectRadii[e[0]],
+                  cstRadSectionEdge ?  std::min(vectRadii[e[0]], vectRadii[e[1]]) : vectRadii[e[1]] };
+     
+     
       Mesh<Z3i::RealPoint>::createTubularMesh(aMesh, vertex, radii, 0.05);
     }
     viewer << CustomColors3D(DGtal::Color::Black, edgeColor);
